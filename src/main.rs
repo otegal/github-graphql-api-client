4f0e0d1d204e48e1a5f5142171f3365f5726e4e4
otegal <gyouzakuuka@gmail.com>
2022-03-31 01:24:35 +0900

    User-Agentを指定の方法で設定しないと403エラーになるため、セットした

--- src/main.rs
+++ src/main.rs
@@ -5,12 +5,15 @@ use std::env;
 async fn main() -> anyhow::Result<()> {
     dotenv().ok();
     let token = env::var("GITHUB_ACCESS_TOKEN")?;
-    println!("token: {:?}", token);
+    let github_user_name = env::var("GITHUB_USER_NAME")?;
 
     let client = reqwest::Client::new();
     let res = client
         .post("https://api.github.com/graphql")
         .header("Authorization", format!("bearer {}", token))
+        // User-Agentに指定の値を入れない場合403が返ってくる
+        // cf. https://docs.github.com/ja/rest/overview/resources-in-the-rest-api#user-agent-required
+        .header("User-Agent", github_user_name)
         .send()
         .await?;
 
