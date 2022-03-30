a436731fcc64d57d3d57f524f629589919c69da2
otegal <gyouzakuuka@gmail.com>
2022-03-31 00:18:19 +0900

    APIコール時にトークンが必要であるため、.envからトークンを取得できる様にした

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
-fn main() {
-    println!("Hello, world!");
+use dotenv::dotenv;
+use std::env;
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    dotenv().ok();
+    let token = env::var("GITHUB_ACCESS_TOKEN")?;
+    println!("token: {:?}", token);
+
+    Ok(())
 }
