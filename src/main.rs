eab7d89bfa78b051d6d60858782c7aa8ffd34cae
otegal <gyouzakuuka@gmail.com>
2022-03-31 02:00:09 +0900

    クエリを使ってデータ取得ができることを確認したかったので、Raw string literalを使ってリクエストできるようにした。Raw string literalを利用する場合、json()では`Body should be a JSON object`とエラーがレスポンスされるのでbodyを利用してJSON形式の文字列をそのままPOSTさせている

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,9 @@ async fn main() -> anyhow::Result<()> {
     let token = env::var("GITHUB_ACCESS_TOKEN")?;
     let github_user_name = env::var("GITHUB_USER_NAME")?;
 
+    // 一旦Raw string literalを利用してクエリを組む
+    let query = r#"{ "query": "query { viewer { login }}" }"#;
+
     let client = reqwest::Client::new();
     let res = client
         .post("https://api.github.com/graphql")
@@ -14,11 +17,10 @@ async fn main() -> anyhow::Result<()> {
         // User-Agentに指定の値を入れない場合403が返ってくる
         // cf. https://docs.github.com/ja/rest/overview/resources-in-the-rest-api#user-agent-required
         .header("User-Agent", github_user_name)
+        .body(query)
         .send()
         .await?;
 
-    println!("{:?}", res);
-    println!("----------------");
     println!("{:?}", res.text().await?);
     Ok(())
 }
