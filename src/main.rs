d313583458f0d84f79eb76eba5faa9e217a32efb
otegal <gyouzakuuka@gmail.com>
2022-04-02 17:18:47 +0900

    GraphQLの型を全て構造体で定義するのは辛いので、graphql-clientでスキーマ情報を読み取り型を生成して取り扱える様にした。それでサンプルをして1つクエリを投げてみた

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,19 @@
 use dotenv::dotenv;
 use std::env;
+use graphql_client::GraphQLQuery;
+use graphql_client::reqwest::post_graphql;
+use reqwest::Client;
+
+#[allow(clippy::upper_case_acronyms)]
+type URI = String;
+
+#[derive(GraphQLQuery)]
+#[graphql(
+    schema_path = "schema.docs.graphql",
+    query_path = "query.graphql",
+    response_derives = "Debug"
+)]
+struct RepoView;
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
@@ -7,20 +21,38 @@ async fn main() -> anyhow::Result<()> {
     let token = env::var("GITHUB_ACCESS_TOKEN")?;
     let github_user_name = env::var("GITHUB_USER_NAME")?;
 
-    // 一旦Raw string literalを利用してクエリを組む
-    let query = r#"{ "query": "query { viewer { login }}" }"#;
+    // // 一旦Raw string literalを利用してクエリを組む
+    // let query = r#"{ "query": "query { viewer { login }}" }"#;
+    //
+    // let client = reqwest::Client::new();
+    // let res = client
+    //     .post("https://api.github.com/graphql")
+    //     .header("Authorization", format!("bearer {}", token))
+    //     // User-Agentに指定の値を入れない場合403が返ってくる
+    //     // cf. https://docs.github.com/ja/rest/overview/resources-in-the-rest-api#user-agent-required
+    //     .header("User-Agent", github_user_name)
+    //     .body(query)
+    //     .send()
+    //     .await?;
+    //
+    // println!("{:?}", res.text().await?);
 
-    let client = reqwest::Client::new();
-    let res = client
-        .post("https://api.github.com/graphql")
-        .header("Authorization", format!("bearer {}", token))
-        // User-Agentに指定の値を入れない場合403が返ってくる
-        // cf. https://docs.github.com/ja/rest/overview/resources-in-the-rest-api#user-agent-required
-        .header("User-Agent", github_user_name)
-        .body(query)
-        .send()
-        .await?;
+    let variables = repo_view::Variables{
+        owner: "otegal".into(),
+        name:  "github-graphql-api-client".into(),
+    };
+    let client = Client::builder()
+        .user_agent(github_user_name)
+        .default_headers(
+            std::iter::once((
+                reqwest::header::AUTHORIZATION,
+                reqwest::header::HeaderValue::from_str(&format!("bearer {}", token))?
+            ))
+            .collect(),
+        )
+        .build()?;
 
-    println!("{:?}", res.text().await?);
+    let res = post_graphql::<RepoView, _>(&client, "https://api.github.com/graphql", variables).await?;
+    println!("{:?}", res);
     Ok(())
 }
